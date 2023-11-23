5b80dee2a6decb54453fb946a63c319625bc932d
gb <gabriel.cayres.r@gmail.com>
2023-11-23 14:00:34 -0300

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 const url = "https://api.adviceslip.com/advice";
+
+const $adviceId = document.querySelector("#id");
+const $advice = document.querySelector("#quote");
+const $dice = document.querySelector("#dice");
+
+async function fetchData() {
+    try {
+        $advice.textContent = "let me think..."
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data.slip)
+        return data.slip;
+    } catch(error) {
+        console.log("Error:" + error);
+    }
+}
+
+async function displayAdvice(data) {
+    $adviceId.textContent = `advice #${data.id}`;
+    $advice.textContent = data.advice;
+}
+
+async function fetchDataAndDisplayAdvice() {
+    const data = await fetchData(url)
+    console.log(data);
+    displayAdvice(data);
+}
+
+$dice.addEventListener('click', fetchDataAndDisplayAdvice);
